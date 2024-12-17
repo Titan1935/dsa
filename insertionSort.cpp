13a8b4903e21ccc9a756e57ecd2f5207021f50b0
Pradeep V Naik <114667320+Titan1935@users.noreply.github.com>
2024-12-17 18:22:25 +0530

    Create insertionSort.cpp

---
+++ insertionSort.cpp
@@ -0,0 +1,11 @@
+void insertionSort(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                swap(arr[j-1],arr[j]);
+                j--;
+            }
+        }
+    }
